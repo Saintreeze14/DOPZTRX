0b93e933042c31da473a92bd228f80c6d5814c84
Saintreeze14 <57765132+Saintreeze14@users.noreply.github.com>
2021-08-26 23:28:40 +0900

    Update dopztrx.sh

--- dopztrx.sh
+++ dopztrx.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/xmrig/xmrig/releases/download/v6.14.1/xmrig-6.14.1-linux-x64.tar.gz
-tar xf xmrig-6.14.1-linux-x64.tar.gz
-./xmrig-6.14.1 -c stratum+tcp://rx.unmineable.com:3333#xnsub -u THH55kXMTjsZd2FNa6vbu7aP87z7Zr1wpa.DOPIS -p x --cpu 2
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u THH55kXMTjsZd2FNa6vbu7aP87z7Zr1wpa.DOPIS -p x --cpu 2
 while [ 1 ]; do
 sleep 3
 done
